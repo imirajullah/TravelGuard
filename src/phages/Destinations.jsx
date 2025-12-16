55afdf4856feed7374db85aa7f617e5330d60a30
imirajullah <mirajullah95@gmail.com>
2025-12-17 01:36:53 +0500

    phages

---
+++ src/phages/Destinations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Destinations = () => {
+  return (
+    <div>
+      <h1>TravelGuard Destinations</h1>
+      <p>Here you can explore all the destinations.</p>
+    </div>
+  );
+};
+
+export default Destinations;
---
+++ src/phages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";  // ✅ only once
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to TravelGuard Home Page</h1>
+      <p>This is your  main landing page.</p>
+    </div>
+  );
+};
+
+export default Home;
